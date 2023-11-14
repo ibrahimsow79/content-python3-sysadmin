c56bb6dcf09bf439c0ce95e274e81a3f7f6ce3ac
ibrahimsow79 <ibrahimsow79@gmail.com>
2023-11-14 09:33:05 +0100

    added argparse module

---
+++ param.echo.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3.8
+import sys
+print(f"the first parameter {sys.argv[0]}")
+print(f"First argument {sys.argv[0]}")
---
+++ param_echo.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3.8
+import sys
+print(f"Positional argument {sys.argv[1:]}")
+print(f"the first parameter is {sys.argv[1]}")
