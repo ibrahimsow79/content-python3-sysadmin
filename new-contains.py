72704c3591a405bf422011b559abb872594f949b
ibrahimsow79 <ibrahimsow79@gmail.com>
2023-12-06 21:42:05 +0100

    commit du 6 décembre 2023

---
+++ new-contains.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.8
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Search for words including partial words",
+    epilog="Text at the bottom of the help")
+parser.add_argument('snippet', help='partial or complete snippet to search for in words')
+args = parser.parse_args()
+snippet = args.snippet.lower()
+
+with open('/usr/share/dict/words') as f:
+    words = f.readlines()
+
+matches = [] 
+matches = [ word.strip() for word in words if snippet in word.lower()] 
+
+print(matches)
